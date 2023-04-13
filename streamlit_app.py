0af218e13cbe079dbb87239532031156f40acaff
grace01smith <41296076+grace01smith@users.noreply.github.com>
2023-04-13 19:09:34 +1200

    Update streamlit_app.py
    fixed a bug

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,4 @@ streamlit.header('Breakfast Menu')
 streamlit.write('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.write('🥗 Kale, Spinach 3 & Rocket Smoothie')
 streamlit.write('🐔 Hard-boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast'
+streamlit.text('🥑🍞 Avocado Toast')
